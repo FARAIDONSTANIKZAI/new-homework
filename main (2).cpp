b5533e437f00e63be4785dc49dcdbb31d9a5881b
FARAIDON STANIKZAI <49916724+FARAIDONSTANIKZAI@users.noreply.github.com>
2019-07-15 12:17:06 +0430

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int inch ;
+   double centimeters ;
+   cout <<"this program convert inches to centimeters"<<endl;
+   cout<<"inter the value of inch :"<<endl ;
+   cin>> inch ;
+   centimeters = 2.54*inch ;
+   cout << inch <<" inch =  "  << centimeters  <<" centimeters " ;
+
+    return 0;
+}
